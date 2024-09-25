e8a67a99dd0ec60d689aca80c83177387435c350
Mohamed Rayane Bouzroura <2111752@cegepmontpetit.ca>
2024-09-25 13:44:04 -0400

    wow

--- guard/src/app/app-routing.module.ts
+++ guard/src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ParentComponent } from './parent/parent.component';
+import { BonbonComponent } from './bonbon/bonbon.component';
+import { SelComponent } from './sel/sel.component';
+import { CaramelComponent } from './caramel/caramel.component';
 
 const routes: Routes = [
-  { path: '', component: ParentComponent }
+  {
+    path: '', component: ParentComponent, children: [
+      { path: 'bonbon', component: BonbonComponent },
+      { path: 'sel', component: SelComponent },
+      { path: 'caramelAuSel', component: CaramelComponent },
+
+
+    ]
+  }
 ];
 
 @NgModule({
